6d394868ca4ee81c99552fe2a86f89e84fac8d09
Igor Savin <33480219+igor-savin-ht@users.noreply.github.com>
2018-01-15 18:02:07 +0200

    Add failing test for content type validation (#18)
    Add tests for content-type validation

--- test/middleware-test.js
+++ test/middleware-test.js
@@ -378,6 +378,27 @@ describe('input-validation middleware tests', function () {
                     done();
                 });
         });
+        it('bad request - wrong content-type (should be application/json)', function (done) {
+            request(app)
+                .put('/pets')
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send([{
+                    name: 'name',
+                    tag: 'tag',
+                    test: {
+                        field1: 'enum1'
+                    }
+                }])
+                .expect(400, function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    let moreInfoAsJson = JSON.parse(res.body.more_info);
+                    // ToDo not sure what data will arrive in response that could be asserted
+					expect(res.body.more_info).to.not.includes('should be array');
+                    done();
+                });
+        });
     });
     describe('Simple server - with base path', function () {
         var app;
