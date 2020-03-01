3b76c7205b07d024abf0d6cac6fa61fc4c3c39c9
Mario Macai <mario.macai@accenture.com>
2020-03-01 12:54:09 +0100

    updated request validation content-type to strip additional info

--- src/middleware.js
+++ src/middleware.js
@@ -30,9 +30,10 @@ function validate(...args) {
 }
 
 function _validateRequest(requestOptions) {
+    const requestContentType = requestOptions.headers['content-type'] && requestOptions.headers['content-type'].split(';')[0].trim(); // This is to filter out things like charset
     return Promise.all([
         _validateParams(requestOptions.headers, requestOptions.params, requestOptions.query, requestOptions.files, requestOptions.path, requestOptions.method.toLowerCase()).catch(e => e),
-        _validateBody(requestOptions.body, requestOptions.path, requestOptions.method.toLowerCase(), requestOptions.headers['content-type']).catch(e => e)
+        _validateBody(requestOptions.body, requestOptions.path, requestOptions.method.toLowerCase(), requestContentType).catch(e => e)
     ]).then(function (errors) {
         if (errors[0] || errors[1]) {
             return errors[0] && errors[1] ? Promise.reject(errors[0].concat(errors[1])) : errors[0] ? Promise.reject(errors[0]) : Promise.reject(errors[1]);
